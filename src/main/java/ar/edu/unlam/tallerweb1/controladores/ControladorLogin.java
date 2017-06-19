8e417381d699fe2070aa457c81549f501f89dd8f
Sebastian Ismael <sebastianismael@gmail.com>
2017-06-19 10:49:44 -0300

    Comenta todo el codigo

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorLogin.java
@@ -15,35 +15,51 @@ import ar.edu.unlam.tallerweb1.servicios.ServicioLogin;
 @Controller
 public class ControladorLogin {
 
+	// La anotacion @Inject indica a Spring que en este atributo se debe setar (inyeccion de dependencias)
+	// un objeto de una clase que implemente la interface ServicioLogin, dicha clase debe estar anotada como
+	// @Service o @Repository y debe estar en un paquete de los indicados en applicationContext.xml
 	@Inject
 	private ServicioLogin servicioLogin;
-	
+
+	// Este metodo escucha la URL localhost:8080/NOMBRE_APP/login si la misma es invocada por metodo http GET
 	@RequestMapping("/login")
 	public ModelAndView irALogin() {
 
 		ModelMap modelo = new ModelMap();
+		// Se agrega al modelo un objeto del tipo Usuario en con key 'usuario' para que el mismo sea asociado
+		// al model attribute del form que esta definido en la vista 'login'
 		Usuario usuario = new Usuario();
 		modelo.put("usuario", usuario);
+		// Se va a la vista login (el nombre completo de la lista se resuelve utilizando el view resolver definido en el archivo spring-servlet.xml)
+		// y se envian los datos a la misma  dentro del modelo
 		return new ModelAndView("login", modelo);
 	}
 
+	// Este metodo escucha la URL validar-login siempre y cuando se invoque con metodo http POST
+	// El método recibe un objeto Usuario el que tiene los datos ingresados en el form correspondiente y se corresponde con el modelAttribute definido en el
+	// tag form:form
 	@RequestMapping(path = "/validar-login", method = RequestMethod.POST)
 	public ModelAndView validarLogin(@ModelAttribute("usuario") Usuario usuario) {
 		ModelMap model = new ModelMap();
 
+		// invoca el metodo consultarUsuario del servicio y hace un redirect a la URL /home, esto es, en lugar de enviar a una vista
+		// hace una llamada a otro action a través de la URL correspondiente a ésta
 		if (servicioLogin.consultarUsuario(usuario) != null) {
 			return new ModelAndView("redirect:/home");
 		} else {
+			// si el usuario no existe agrega un mensaje de error en el modelo.
 			model.put("error", "Usuario o clave incorrecta");
 		}
 		return new ModelAndView("login", model);
 	}
-	
+
+	// Escucha la URL /home por GET, y redirige a una vista.
 	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public ModelAndView irAHome() {
 		return new ModelAndView("home");
 	}
-	
+
+	// escucha la url /, y redirige a la URL /login, es lo mismo que si se invoca la url /login directamente.
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public ModelAndView inicio() {
 		return new ModelAndView("redirect:/login");
