ada16b19d98a9493b17bf22b77f4dffd4eb0d3b4
Sebastian Ismael <sebastianismael@gmail.com>
2017-06-19 11:39:15 -0300

    Comenta todo el codigo

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioLogin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioLogin.java
@@ -2,6 +2,7 @@ package ar.edu.unlam.tallerweb1.servicios;
 
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
 
+// Interface que define los metodos del Servicio de Usuarios.
 public interface ServicioLogin {
 
 	Usuario consultarUsuario(Usuario usuario);
