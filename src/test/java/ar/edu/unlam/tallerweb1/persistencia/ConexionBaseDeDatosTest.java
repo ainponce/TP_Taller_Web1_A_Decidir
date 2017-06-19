26c06cba94e9eade92c86fa3573341b81b09c5cb
Sebastian Ismael <sebastianismael@gmail.com>
2017-06-19 11:46:47 -0300

    Comenta todo el codigo

---
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.*;
+
+public class ConexionBaseDeDatosTest extends SpringTest{
+
+    @Test
+    @Transactional @Rollback(true)
+    public void pruebaConexion(){
+        assertThat(getSession().isConnected()).isTrue();
+    }
+}
