1bb23c24506a24cdc3566adba5b3dc1d0615bfc9
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-07-19 15:14:20 -0300

    actualiza dependencias para mysql y agrega un test

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
 import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +12,19 @@ import static org.assertj.core.api.Assertions.*;
 public class ConexionBaseDeDatosTest extends SpringTest{
 
     @Test
-    @Transactional @Rollback(true)
+    @Transactional @Rollback
     public void pruebaConexion(){
         assertThat(session().isConnected()).isTrue();
     }
+
+    @Test
+    @Transactional @Rollback
+    public void crearUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("seba@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setRol("ADMIN");
+        session().save(usuario);
+    }
+
 }
