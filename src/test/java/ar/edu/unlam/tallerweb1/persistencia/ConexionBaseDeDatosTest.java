54ddec99afb98fb6e968a8a02d09ed2f1d0dbbd6
Sebastian Ismael <sebastian.ismael@etermax.com>
2021-08-26 20:24:57 -0300

    cambio para que se dispare el build

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -20,6 +20,7 @@ public class ConexionBaseDeDatosTest extends SpringTest{
     @Test
     @Transactional @Rollback
     public void crearUsuario(){
+        // lalalalala
         Usuario usuario = new Usuario();
         usuario.setEmail("seba@gmail.com");
         usuario.setPassword("1234");
