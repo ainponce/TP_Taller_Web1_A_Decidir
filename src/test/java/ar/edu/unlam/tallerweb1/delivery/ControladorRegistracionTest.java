de00514a53d235bc626a1b667a59b09695df1d38
Ain Moises Ponce <ponce.ain@gmail.com>
2023-05-04 20:01:54 -0300

    primer commit

---
+++ src/test/java/ar/edu/unlam/tallerweb1/delivery/ControladorRegistracionTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.tallerweb1.delivery;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ControladorRegistracionTest {
+
+
+    @Test
+    public void alIngresarCredencialesCorrectasDeUnUsuarioQueNoExisteMeRegistraYLlevaAlLogin(){
+       //Corre y una contraseña valida
+
+       //Preparación
+       dadoQueNoExisteElUsuario();
+       //Ejecución
+        ModelAndView mav =
+       //Verificación
+    }
+}
