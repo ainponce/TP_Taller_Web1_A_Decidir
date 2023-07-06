fb24bfc880fa69af30dae28760dfc142f59ba573
Ain Moises Ponce <ponce.ain@gmail.com>
2023-07-05 23:29:40 -0300

    creaciÃon de clase ahorro, servicio y vista

---
+++ src/main/java/ar/edu/unlam/tallerweb1/delivery/Ahorro/ControladorAhorro.java
@@ -0,0 +1,47 @@
+package ar.edu.unlam.tallerweb1.delivery.Ahorro;
+
+import ar.edu.unlam.tallerweb1.domain.Ahorro.Ahorro;
+import ar.edu.unlam.tallerweb1.domain.Ahorro.ServicioDeAhorro;
+import ar.edu.unlam.tallerweb1.domain.Presupuesto.Presupuesto;
+import ar.edu.unlam.tallerweb1.domain.Presupuesto.ServicioDePresupuesto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ControladorAhorro {
+
+    private final ServicioDePresupuesto servicioDePresupuesto;
+    private final ServicioDeAhorro servicioDeAhorro;
+
+    @Autowired
+    public ControladorAhorro(ServicioDePresupuesto servicioDePresupuesto, ServicioDeAhorro servicioDeAhorro) {
+        this.servicioDePresupuesto = servicioDePresupuesto;
+        this.servicioDeAhorro = servicioDeAhorro;
+    }
+
+    @RequestMapping(path = "/calculadoraDeAhorro", method = RequestMethod.GET)
+    public ModelAndView mostrarVistaDeCalculadoraDeAhorro(){
+        ModelMap map = new ModelMap();
+        List<Presupuesto> prespuesto = servicioDePresupuesto.listarPresupuestos();
+        map.put("listaDePresupuesto", prespuesto);
+        map.put("ListarAhorroPordia", new Ahorro());
+        return new ModelAndView("calculadoraDeAhorro", map);
+    }
+
+    @RequestMapping( path="/calculadoraDeAhorro", method = RequestMethod.POST)
+    public ModelAndView calcularAhorro(@RequestParam("nombreAhorro")String nombreAhorro,
+                                       @RequestParam("montoAhorro")double montoAhorro,
+                                       @RequestParam("presupuesto")double montoDelPrespuesto){
+        ModelMap map = new ModelMap();
+        double ahorroPorDia =servicioDeAhorro.calcularAhorroPorDia(montoDelPrespuesto, montoAhorro);
+        map.put("ahorroPorDia", ahorroPorDia);
+        return new ModelAndView("calculadoraDeAhorro", map);
+    }
+}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/Ahorro/ServicioDeAhorro.java
@@ -0,0 +1,6 @@
+package ar.edu.unlam.tallerweb1.domain.Ahorro;
+
+public interface ServicioDeAhorro {
+
+    public Double calcularAhorroPorDia(double montoPresupuesto, double montoAhorro);
+}
